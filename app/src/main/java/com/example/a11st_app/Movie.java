3a045b94bb914f51d2a645dbfd3e747db4c828d5
bit <2001vit@naver.com>
2022-05-24 16:15:05 +0900

    영화 정보의 수 추가

---
+++ app/src/main/java/com/example/a11st_app/Movie.java
@@ -0,0 +1,22 @@
+package com.example.a11st_app;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rankOldAndNew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String salesAmt;
+    String salesSheare;
+    String salesInten;
+    String salesChange;
+    String salesAcc;
+    String audiCnt;
+    String audiInten;
+    String audiChange;
+    String audiAcc;
+    String scrnCnt;
+    String showCnt;
+}
